9dd5ed7e8838d565a0e5f383c1abd2325d1d9dcf
codewithamitpatil <pamit8831@gmail.com>
2022-07-28 13:13:01 +0530

    initial commit 22

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = process.env.NODEPORT || 5000;
+const cors = require('cors');
+const app = express();
+
+app.use(cors('*'));
+
+
+app.get('', async (req, res) => {
+    res.send('welcome from amit new serce');
+})
+
+app.listen(port, () => {
+    console.log(`server is running at :: ${process.env.HOST} // ${port}`);
+})
